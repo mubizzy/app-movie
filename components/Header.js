ae23e450c3485e9ec4f44febbf5cf47c21ebb37b
Mubarak Ajibola <ajibolamubarak304@gmail.com>
2022-12-19 18:22:57 +0100

    first commit

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <section className="bg-[#292929] mx-auto px-4 py-6">
+      <div className="container text-center lg:text-left ">
+        <button class="bg-[#000000] text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded ">
+          MyTestApp
+        </button>
+      </div>
+    </section>
+  );
+};
+
+export default Header;
